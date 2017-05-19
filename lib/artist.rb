4b1257542db7037e2fd66893a2d6618d7eb46f7e
sarahsweat <sarahsweat@gmail.com>
2017-05-18 21:57:46 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+
+require "pry"
+
+class Artist
+
+  attr_accessor :name
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(name)
+    self.songs << name
+    name.artist = self
+    @@song_count +=1
+  end
+
+  def add_song_by_name(name)
+    #binding.pry
+    song = Song.new(name)
+    song.artist = self
+    self.songs << song
+    @@song_count +=1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+end
---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+
+  attr_accessor :title,  :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+
+    if author
+      author.name
+    else
+      nil
+    end
+  end
+
+
+end
